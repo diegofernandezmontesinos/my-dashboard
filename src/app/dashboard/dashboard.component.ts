92d52cd3f6ce859294e0fdf67f2c4f4477d69cb5
diego <dfmontesinos@gmail.com>
2024-05-03 20:46:37 +0200

    continue coding

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import ShareSidemenuComponent from './../share/share-sidemenu/share-sidemenu.component';
 
 @Component({
   standalone: true,
-  imports: [ RouterModule],
-  selector: 'app-dashboard',
+  imports: [RouterModule, ShareSidemenuComponent],
   templateUrl: './dashboard.component.html',
 })
 export default class DashboardComponent {}
--- src/app/share/share-sidemenu/share-sidemenu.component.ts
+++ src/app/share/share-sidemenu/share-sidemenu.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
+  standalone: true,
   selector: 'app-share-sidemenu',
   templateUrl: './share-sidemenu.component.html',
-  styleUrls: ['./share-sidemenu.component.css']
+  styleUrls: ['./share-sidemenu.component.css'],
 })
-export class ShareSidemenuComponent implements OnInit {
+export default class ShareSidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {
+    // const dashboardRouter = routes
+    //   .map((route) => route.children ?? [])
+    //   .flat()
+    //   .filter((route) => route && route.path)
+    //   .filter((route) => !route.path?.includes(':'));
   }
-
 }
