40ddc866beb83c001de36c3753260bd29e3951ed
diego <dfmontesinos@gmail.com>
2024-05-11 13:31:58 +0200

    continue with the for loop

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -11,6 +11,10 @@ export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
 
+  public frameworks = signal(['Angular', 'Vue', 'React']);
+
+  public frameworks2 = signal([]);
+
   public toggleContent() {
     this.showContent.update((value) => !value);
   }
